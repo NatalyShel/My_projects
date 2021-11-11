2f3631533005479829490149b3dff47e1bf3c0a1
NatalyShel <tillotama@tut.by>
2021-11-11 20:03:41 +0300

    My HW to L1

---
+++ Nataly_2_15.py
@@ -0,0 +1,4 @@
+# program to elevate float argument
+x = 2014.0
+y = x ** 14
+print("Elevation float value ", x, "to 14 level =", y)
---
+++ Nataly_2_4.py
@@ -0,0 +1,6 @@
+john = 2
+mary = 3
+adam = 4
+print(john, mary, adam, sep=",")
+total_apples = john+mary+adam
+print("Total number of apples: ", total_apples)
